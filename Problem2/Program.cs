6c14d5f91504a535910212b656f1b553a6d77c08
matt <info@mattevans.io>
2020-12-21 10:52:48 -0800

    Add project files.

---
+++ Problem2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Problem2 {
+	class Program {
+		static void Main(string[] args) {
+
+			long sum = 0;
+			long result = 0;
+			long fib1 = 1;
+			long fib2 = 1;
+
+			while (sum < 4000000) {
+
+				if ((result % 2) == 0) {
+
+					sum += result;
+				}
+
+				result = fib1 + fib2;
+				fib2 = fib1;
+				fib1 = result;
+			}
+			Console.WriteLine(sum);
+		}
+	}
+}
---
+++ Problem3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem3 {
+	class Program {
+		static void Main(string[] args) {
+
+			long num = 600851475143;
+			long max = 0;
+			int maxFactor = Convert.ToInt32(Math.Floor(Math.Sqrt(num)));
+
+			for (long i = 2; i < maxFactor; i++) {
+
+				if(num % i == 0) {
+
+					bool isPrime = true;
+
+					for(long j = 2; j < i; j++) {
+
+						if(i % j == 0) {
+
+							isPrime = false;
+							break;
+						}
+					}
+					if(isPrime) {
+						max = i;
+					}
+				}
+			}
+			Console.WriteLine(max);
+		}
+	}
+}
