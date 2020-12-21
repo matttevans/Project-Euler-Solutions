1dfd3f12129ef98a09c2daed432d9457db2f2e59
matt <info@mattevans.io>
2020-12-21 11:54:03 -0800

    Add problem 6

---
+++ Problem6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Problem6 {
+	class Program {
+		static void Main(string[] args) {
+
+
+			int sumOfSquares = 0;
+			int squareOfSum = 0;
+
+			for(int i = 1; i < 101; i++) {
+
+				sumOfSquares += Convert.ToInt32(Math.Pow(i, 2));
+				squareOfSum += i;
+			}
+			Console.WriteLine(Math.Pow(squareOfSum, 2) - sumOfSquares);
+		}
+	}
+}
