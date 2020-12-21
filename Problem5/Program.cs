7a27717a9c712c4b77f8bc80d8ca18e10de4de46
matt <info@mattevans.io>
2020-12-21 11:47:53 -0800

    Add problem 5

---
+++ Problem5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Problem5 {
+
+	class Program {
+
+		static void Main(string[] args) {
+
+			bool end = true;
+			int result = 20;
+
+			while(end) {
+
+				int counter = 0;
+				Console.WriteLine(result);
+
+				for (int i = 21; i > 1; i--) {
+
+					if (result % i == 0) {
+						counter++;
+					}
+					else {
+						counter = 0;
+						break;
+					}
+					if (counter == 20) {
+
+						Console.WriteLine(result);
+						end = false;
+					}
+				}
+				result+=20;
+			}
+		}
+	}
+}
